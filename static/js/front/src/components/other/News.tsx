2442403cbe8ded8c14fc914124f835f5c6ef6675
Davis-Software <davissoftware6@gmail.com>
2023-08-28 14:48:41 +0200

    added pagination support to the commit view on the news page

--- static/js/front/src/components/other/News.tsx
+++ static/js/front/src/components/other/News.tsx
@@ -1,17 +1,18 @@
 import React, {useEffect} from "react";
-import {Button, Container} from "@mui/material";
+import {Button, ButtonGroup, Container} from "@mui/material";
 import SwcModal from "../SwcModal";
 import PageLoader from "../PageLoader";
 
 function CommitHistory(){
     const [commits, setCommits] = React.useState<any[]>([]);
+    const [page, setPage] = React.useState<number>(1);
 
     useEffect(() => {
-        fetch('https://api.github.com/repos/Davis-Software/swc_flexnit/commits')
+        fetch(`https://api.github.com/repos/Davis-Software/swc_flexnit/commits?per_page=30&page=${page}`)
             .then(res => res.json())
             .then(setCommits)
             .catch(console.error)
-    }, []);
+    }, [page]);
 
     return (
         <Container className="pt-5 p-3">
@@ -27,6 +28,14 @@ function CommitHistory(){
                     ))}
                 </ul>
             ) : <PageLoader />}
+
+            <div className="w-100 d-flex justify-content-center">
+                <ButtonGroup>
+                    <Button disabled={page === 1} onClick={() => setPage(page - 1)}>Previous</Button>
+                    <Button disabled={true}>{page}</Button>
+                    <Button disabled={commits.length < 30} onClick={() => setPage(page + 1)}>Next</Button>
+                </ButtonGroup>
+            </div>
         </Container>
     )
 }
