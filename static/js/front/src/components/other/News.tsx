58f981d74a66cf9a2c79ac1e800abd875f4550b4
Davis-Software <davissoftware6@gmail.com>
2023-08-28 14:21:39 +0200

    integrated and updated the news component

--- static/js/front/src/components/other/News.tsx
+++ static/js/front/src/components/other/News.tsx
@@ -2,10 +2,28 @@ import React from "react";
 import {Container} from "@mui/material";
 
 function News(){
-    return null
     return (
         <Container className="pt-5 p-3">
-            news content
+            <h3 className="text-center">Latest Features</h3>
+            <hr/>
+
+            <ul>
+
+                <li>Intro location can now be set on a per-episode basis</li>
+                <li>Play next episode button can now be displayed as episode endcard starts</li>
+                <li>Added intro detection for series using <a href="https://projects.software-city.org/p/aivd" target="_blank">AIVD</a></li>
+
+            </ul>
+
+            <h5 className="mt-5">Change history</h5>
+            <hr/>
+
+            <ul>
+
+                <li>Added a intro skip button for episodes</li>
+                <li>Added a play next episode button for episodes</li>
+
+            </ul>
         </Container>
     )
 }
