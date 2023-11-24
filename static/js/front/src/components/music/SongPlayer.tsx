c716b118613effbab4f42ca55e41a9cadc4e6984
Davis-Software <davissoftware6@gmail.com>
2023-11-24 21:48:30 +0100

    added song mods control to song player

--- static/js/front/src/components/music/SongPlayer.tsx
+++ static/js/front/src/components/music/SongPlayer.tsx
@@ -1,5 +1,15 @@
 import React, {useEffect, useState} from "react"
-import {IconButton, Paper, Slider, useTheme} from "@mui/material";
+import {
+    FormControl,
+    IconButton,
+    InputLabel,
+    MenuItem,
+    Paper,
+    Select,
+    SelectChangeEvent,
+    Slider,
+    useTheme
+} from "@mui/material";
 import SongType from "../../types/songType";
 
 interface SongPlayerProps {
@@ -9,6 +19,8 @@ function SongPlayer(props: SongPlayerProps){
     const [playing, setPlaying] = React.useState<boolean>(false)
 
     const audioRef = React.useRef<HTMLAudioElement>(null)
+    const [lockSpeedSelect, setLockSpeedSelect] = React.useState<boolean>(false)
+    const [audioSpeed, setAudioSpeed] = React.useState<string>("1")
     const [volume, setVolume] = useState(parseFloat(localStorage.getItem("music-volume") || "1"))
     const [duration, setDuration] = React.useState<number>(0)
     const [position, setPosition] = React.useState<number>(0)
@@ -20,6 +32,51 @@ function SongPlayer(props: SongPlayerProps){
     }
     function handleSetPosition(){
         setPosition(audioRef.current?.currentTime!)
+    }
+
+    function setNightcoreMode(speed: number, skip_ramp=false){
+        if(!audioRef.current) return
+        if(!skip_ramp){
+            setLockSpeedSelect(true)
+            function advanceValue(){
+                if(!audioRef.current) return;
+                let advance = 0.01
+                if(audioRef.current.playbackRate + advance >= speed){
+                    setLockSpeedSelect(false)
+                    audioRef.current.playbackRate = speed
+                    return
+                }
+                if(audioRef.current?.playbackRate < speed){
+                    audioRef.current.playbackRate += advance
+                }
+                setTimeout(advanceValue, 10)
+            }
+            function reduceValue(){
+                if(!audioRef.current) return;
+                let reduce = 0.01
+                if(audioRef.current.playbackRate - reduce <= speed){
+                    setLockSpeedSelect(false)
+                    audioRef.current.playbackRate = speed
+                    return
+                }
+                if(audioRef.current?.playbackRate > speed){
+                    audioRef.current.playbackRate -= reduce
+                }
+                setTimeout(reduceValue, 10)
+            }
+            if(audioRef.current.playbackRate < speed){
+                advanceValue()
+            }else{
+                reduceValue()
+            }
+        } else {
+            audioRef.current.preservesPitch = false
+            audioRef.current.playbackRate = speed
+        }
+    }
+    function handleSpeedChange(e: SelectChangeEvent){
+        setAudioSpeed(e.target.value)
+        setNightcoreMode(parseFloat(e.target.value))
     }
 
     useEffect(() => {
@@ -38,6 +95,9 @@ function SongPlayer(props: SongPlayerProps){
         audioRef.current.volume = volume
         localStorage.setItem("music-volume", volume.toString())
     }, [volume])
+    useEffect(() => {
+        setNightcoreMode(parseFloat(audioSpeed), true)
+    }, [props.playingSong]);
 
     return (
         <div style={{height: "64px", width: "100%", position: "fixed", bottom: 0}}>
@@ -101,6 +161,32 @@ function SongPlayer(props: SongPlayerProps){
                         step={0.01}
                     />
                 </div>
+                <FormControl
+                    sx={{
+                        width: "200px",
+                    }}
+                    size="small"
+                >
+                    <InputLabel id="nightcore-mode-select-label">Music Mod</InputLabel>
+                    <Select
+                        labelId="nightcore-mode-select-label"
+                        id="nightcore-mode-select"
+                        value={audioSpeed}
+                        label="Nightcore Mode"
+                        onChange={handleSpeedChange}
+                        disabled={lockSpeedSelect}
+                    >
+                        <MenuItem value=".5">Slowdrive (x0.5)</MenuItem>
+                        <MenuItem value=".8">Daycore (x0.8)</MenuItem>
+                        <MenuItem value=".85">Daycore N# (x0.85)</MenuItem>
+                        <MenuItem value="1">Off (x1)</MenuItem>
+                        <MenuItem value="1.2">Nightcore (x1.2)</MenuItem>
+                        <MenuItem value="1.25">Nightcore N# (x1.25)</MenuItem>
+                        <MenuItem value="1.3">Hypercore (x1.3)</MenuItem>
+                        <MenuItem value="1.5">Beatpump (x1.5)</MenuItem>
+                    </Select>
+
+                </FormControl>
             </Paper>
             <audio
                 src={props.playingSong ? `/music/${props.playingSong.uuid}` : undefined}
