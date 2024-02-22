e7b490f139cb24a84370a9ee09f01e819a61d388
Davis-Software <davissoftware6@gmail.com>
2024-02-22 20:21:20 +0100

    changed the loading animation to fit the icon change

--- static/js/front/src/components/SwcLoader.tsx
+++ static/js/front/src/components/SwcLoader.tsx
@@ -1,39 +1,16 @@
 import React from "react"
 
-function Triangle(){
-    return (
-        <svg width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none">
-            <polygon points="0,100 100,0 100,100"/>
-        </svg>
-    )
-}
-
-function DoubleTriangle(){
-    return (
-        <svg width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none">
-            <polygon points="100,0 70,70 0,100 30,30"/>
-        </svg>
-    )
-}
-
 interface SwcLoaderProps{
     className?: string
     style?: React.CSSProperties
+    text?: string
 }
 function SwcLoader(props: SwcLoaderProps){
     return (
         <div className={`swc-loader ${props.className}`} style={props.style}>
-            <div className="swc-loader-inner">
-                <div className="triangle left">
-                    <Triangle />
-                </div>
-                <div className="double-triangle">
-                    <DoubleTriangle />
-                </div>
-                <div className="triangle right">
-                    <Triangle />
-                </div>
-            </div>
+            <img src="/static/img/favicon-256.png" className="box" alt="Loader"/>
+            <div className="shadow"></div>
+            <div className="text">{props.text}</div>
         </div>
     )
 }
