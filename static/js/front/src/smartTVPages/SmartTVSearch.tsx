ba51319e38a7d821019ebffeaf95a0fa86da682a
Davis-Software <davissoftware6@gmail.com>
2024-04-06 20:40:58 +0200

    started implementation of the smartTV search page

--- static/js/front/src/smartTVPages/SmartTVSearch.tsx
+++ static/js/front/src/smartTVPages/SmartTVSearch.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {useFocusable} from "@noriginmedia/norigin-spatial-navigation";
+import SmartTVPageBase from "./SmartTVPageBase";
+import {TextField} from "@mui/material";
+import SmartTVGridTitleList from "../smartTVComponents/titleDisplay/SmartTVGridTitleList";
+import TitleEntryType from "../types/titleEntryType";
 
 function SmartTVHome(){
     const {ref, focused} = useFocusable()
+    const inputRef = useRef<HTMLInputElement>()
+
+    const [searchText, setSearchText] = useState("");
+    const [titles, setTitles] = useState<TitleEntryType[]>([]);
+
+    useEffect(() => {
+        if(focused) {
+            inputRef.current?.focus()
+        }else{
+            inputRef.current?.blur()
+        }
+    }, [focused]);
+
+    useEffect(() => {
+        fetch(`/search/all${searchText !== "" ? "?q=" + searchText : ""}`)
+            .then(res => res.json())
+            .then(setTitles)
+    }, [searchText])
 
     return (
-        <div ref={ref}>
-            {focused ? "focused search" : "TVSearch"}
-        </div>
+        <SmartTVPageBase>
+            <div className="d-flex justify-content-center my-3">
+                <TextField
+                    label="Search"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                    ref={ref}
+                    variant="standard"
+                    sx={{width: "50%"}}
+                    inputRef={inputRef}
+                />
+            </div>
+            <SmartTVGridTitleList titles={titles} setFocusedTitle={() => {}} skeletonAmount={35} />
+        </SmartTVPageBase>
     )
 }
 
