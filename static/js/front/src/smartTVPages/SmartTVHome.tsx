4c465014d67620a0823f587b5f09723390e03a2f
Davis-Software <davissoftware6@gmail.com>
2024-04-06 20:45:40 +0200

    titles on smartTV home page are now sorted by tags

--- static/js/front/src/smartTVPages/SmartTVHome.tsx
+++ static/js/front/src/smartTVPages/SmartTVHome.tsx
@@ -11,11 +11,22 @@ function SmartTVHome(){
     const [focusedTitle, setFocusedTitle] = React.useState<TitleEntryType | null>(null);
     const [libraryTitles, setLibraryTitles] = React.useState<TitleEntryType[]>([]);
     const [titles, setTitles] = React.useState<TitleEntryType[]>([]);
+    const [tags, setTags] = React.useState<string[]>([]);
 
     useEffect(() => {
         fetchUserLibrary().then(setLibraryTitles);
         fetchSearchAll().then(setTitles);
     }, []);
+    useEffect(() => {
+        let tagList: string[] = [];
+        titles.forEach(title => {
+            if(!title.tags || title.tags === "") return;
+            title.tags.split(",").forEach(tag => {
+                if(!tagList.includes(tag)) tagList.push(tag);
+            })
+        })
+        setTags(tagList);
+    }, [titles]);
 
     return (
         <SmartTVPageBase>
@@ -27,7 +38,25 @@ function SmartTVHome(){
                     setFocusedTitle={setFocusedTitle}
                 />
             )}
-            <SmartTVGridTitleList name="More Titles" titles={titles} setFocusedTitle={setFocusedTitle} />
+            {tags.length > 0 ? (
+                <>
+                    {tags.map(tag => (
+                        <SmartTVGridTitleList
+                            key={tag}
+                            name={tag}
+                            titles={titles.filter(title => title.tags?.split(",").includes(tag))}
+                            setFocusedTitle={setFocusedTitle}
+                        />
+                    ))}
+                    <SmartTVGridTitleList
+                        name="Other Titles"
+                        titles={titles.filter(title => !title.tags || title.tags === "")}
+                        setFocusedTitle={setFocusedTitle}
+                    />
+                </>
+            ) : (
+                <SmartTVGridTitleList name="More Titles" titles={titles} setFocusedTitle={setFocusedTitle} />
+            )}
         </SmartTVPageBase>
     )
 }
